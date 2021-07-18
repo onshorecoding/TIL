f47622644218724697cf80e9b821f111e3541597
onshore <onshorecoding9@gmail.com>
2021-07-18 21:19:19 +0900

    210718: baekjoon s2 1929

---
+++ algorithm/baekjoon/1929[수학]소수 구하기.py
@@ -0,0 +1,14 @@
+m, n = map(int, input().split())
+
+check = [True] * (n + 1)
+
+e = int((n) ** 0.5)
+
+for i in range(2, e + 1):
+    if check[i] == True:
+        for j in range(i * 2, n + 1, i):
+            check[j] = False
+
+ans = [str(i) for i in range(m, n + 1) if check[i] == True]
+
+print("\n".join(ans))
