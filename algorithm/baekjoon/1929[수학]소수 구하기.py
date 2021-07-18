439c67bcbb5e30c29899e8fbca4c53bfdcdb55d5
onshore <onshorecoding9@gmail.com>
2021-07-18 21:25:01 +0900

    210718: baekjoon s2 1929

--- algorithm/baekjoon/1929[수학]소수 구하기.py
+++ algorithm/baekjoon/1929[수학]소수 구하기.py
@@ -9,6 +9,6 @@ for i in range(2, e + 1):
         for j in range(i * 2, n + 1, i):
             check[j] = False
 
-ans = [str(i) for i in range(m, n + 1) if check[i] == True]
+ans = [str(i) for i in range(m, n + 1) if check[i] == True and i > 1]
 
 print("\n".join(ans))
